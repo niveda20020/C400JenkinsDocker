d86191f27810a7de2221a54a4a6a352187d43977
niveda20020 <nivedasirivelu2003@gmail.com>
2024-10-05 12:42:06 +0530

    commit

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+import unittest
+
+from flaskapp import app
+
+class TestApp(unittest.TestCase):
+    
+        def setUp(self):
+            self.app = app.test_client()
+    
+        def test_hello_world(self):
+            response = self.app.get('/')
+            self.assertEqual(response.data, b'Hello Niveda!')
+
+if __name__ == '__main__':
+    unittest.main()
